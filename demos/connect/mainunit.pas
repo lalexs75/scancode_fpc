fd92805b45422896affe9df50bb76ae8e3608a86
lalexs75 <alexs75@yandex.ru>
2019-10-31 16:45:59 +0300

    Начат пример работы

---
+++ demos/connect/mainunit.pas
@@ -0,0 +1,30 @@
+unit MainUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ScancodeMT;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ScancodeMT1: TScancodeMT;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
