8c7309a479af2429224727afd721030e138268f0
lalexs75 <alexs75@yandex.ru>
2020-07-27 14:46:39 +0300

    xsd cправочники - GetData

--- demos/xml/test_xml_xsd.sh
+++ demos/xml/test_xml_xsd.sh
@@ -1,4 +1,4 @@
-#xmllint --schema yourxsd.xsd yourxml.xml --noout
 xmllint --schema ../../xsd/GetUsers.xsd 01_GetUsers.xml --noout
 xmllint --schema ../../xsd/GetDocum.xsd 02_GetDocum.xml --noout
+xmllint --schema ../../xsd/GetData.xsd 03_GetData.xml --noout
 xmllint --schema ../../xsd/GetStock.xsd 05_GetStock.xml --noout
