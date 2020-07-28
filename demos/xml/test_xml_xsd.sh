51d86e2e463eb125fb36145c430958a7be40f63c
lalexs75 <alexs75@yandex.ru>
2020-07-28 10:05:45 +0300

    XSD запросы статусов товара

--- demos/xml/test_xml_xsd.sh
+++ demos/xml/test_xml_xsd.sh
@@ -3,3 +3,8 @@ xmllint --schema ../../xsd/GetDocum.xsd 02_GetDocum.xml --noout
 xmllint --schema ../../xsd/GetData.xsd 03_GetData.xml --noout
 xmllint --schema ../../xsd/GetStock.xsd 05_GetStock.xml --noout
 xmllint --schema ../../xsd/PutDocum.xsd 06_PutDocum.xml --noout
+
+xmllint --schema ../../xsd/GetProd_0.xsd 10_GetProd_0.xml --noout
+xmllint --schema ../../xsd/GetProd_1.xsd 10_GetProd_1.xml --noout
+xmllint --schema ../../xsd/GetProd_1_answer.xsd 10_GetProd_1_answer.xml --noout
+xmllint --schema ../../xsd/GetProd_2_answer.xsd 10_GetProd_2_answer.xml --noout
