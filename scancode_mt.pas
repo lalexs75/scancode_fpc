1b8e59e6588eb59f71f99d33d3f622ff110d25c8
lalexs75 <alexs75@yandex.ru>
2020-07-28 10:10:20 +0300

    файлы добавлены в пакет

--- scancode_mt.pas
+++ scancode_mt.pas
@@ -11,7 +11,8 @@ uses
   scancode_user_api, scancode_stock_api, scancode_document_api, 
   scancode_characteristics_api, scancode_tsd_order_api, ScancodeMT, 
   ScancodeMT_API, ScancodeMT_utils, GetUsers, GetStock, GetDocum, GetData, 
-  PutDocum, LazarusPackageIntf;
+  PutDocum, GetProd_0, GetProd_1, GetProd_1_answer, GetProd_2_answer, 
+  LazarusPackageIntf;
 
 implementation
 
