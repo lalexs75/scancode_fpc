9d7868ef7e412d566452c162ced34d346410b676
lalexs75 <alexs75@yandex.ru>
2020-08-04 14:57:59 +0300

    Доработка интефейса команды 03_GetData - версия 1.2.17

--- demos/xml/test_xml_xsd.sh
+++ demos/xml/test_xml_xsd.sh
@@ -1,6 +1,7 @@
 xmllint --schema ../../xsd/GetUsers.xsd 01_GetUsers.xml --noout
 xmllint --schema ../../xsd/GetDocum.xsd 02_GetDocum.xml --noout
 xmllint --schema ../../xsd/GetData.xsd 03_GetData.xml --noout
+xmllint --schema ../../xsd/GetData.xsd 03_GetData_02.xml --noout
 xmllint --schema ../../xsd/GetStock.xsd 05_GetStock.xml --noout
 xmllint --schema ../../xsd/PutDocum.xsd 06_PutDocum.xml --noout
 
